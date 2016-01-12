f1761bd6406dcc1404354c64cbf90f5e75139996
Ismael Navarro Páez <ismanapa@gmail.com>
2016-01-12 20:05:59 +0100

    Now we can edit and create groups

--- js/wp_info_panels_admin.js
+++ js/wp_info_panels_admin.js
@@ -3,11 +3,92 @@ jQuery(document).ready(function($) {
 
 	$(".wip_new_group_panel").click(function() {
 		$(".wip_new_group_panel_form").toggle(800);
+		if($(this).find("span").hasClass("glyphicon-plus"))
+			$(this).find("span").switchClass("glyphicon-plus", "glyphicon-minus");
+		else
+			$(this).find("span").switchClass("glyphicon-minus", "glyphicon-plus");
 	});
 
-	$("#wip_new_group").submit(function(event) {
+	$(".wip_update_group").submit(function(event) {
 		event.preventDefault();
-		console.log($(this).serialize());
+		var form_data = $(this).serialize();
+		
+		var data = {
+			'action': 'create_group',
+			'form': form_data
+		};
+
+		$.post(ajaxurl, data, function(response) {
+			console.log(response);
+			if(response == 1){
+				if(form_data.wip_update_group === "0")
+					$(".wip_new_group_panel_form").find(".panel").insert_after_panel_info("El grupo ha sido creado correctamente", "fadeOut", "success");
+				else
+					$(".info_group").find(".panel").insert_after_panel_info("El grupo ha sido actualizado correctamente", "fadeOut", "success");
+			}
+		});
 	});
 
+	$(".groups").delegate(".list-group-item", "click", function(event) {
+		event.preventDefault();
+		
+		if($(this).hasClass("active")){
+			$(this).removeClass("active");
+
+			if($(".info_group").css("display") === "block")
+				$(".info_group").toggle("slide", 800);
+		}
+		else{
+			$(this).closest(".groups").find(".active").removeClass("active");
+			$(this).addClass("active");	
+
+			$group = $(this);
+
+			if($(".info_group").css("display") === "block"){
+				$(".info_group").toggle("slide", 800, function() {
+					$(this).find("#wip_panel_name").val($group.html());
+					$(this).find("#wip_panel_sizex").val($group.data("sizex"));
+					$(this).find("#wip_panel_sizey").val($group.data("sizex"));
+					$(this).find("#wip_group_id").val($group.data("id"));
+					$(".info_group").toggle("slide", 800);
+				});
+			}
+			else{
+				$(".info_group").find("#wip_panel_name").val($group.html());
+				$(".info_group").find("#wip_panel_sizex").val($group.data("sizex"));
+				$(".info_group").find("#wip_panel_sizey").val($group.data("sizex"));
+				$(".info_group").find("#wip_group_id").val($group.data("id"));
+				$(".info_group").toggle("slide", 800);
+			}
+			
+		}
+	});
+
+	//Script para cerrar los paneles de info
+	 $('*').delegate('.clickable', 'click',function(){
+        var effect = $(this).data('effect');
+        $(this).closest('.panel')[effect]();
+    });
+
+});
+
+jQuery.fn.extend({
+	"serialize": function() {
+		var obj = {};
+		jQuery(this).find("input").each(function() {
+			if(jQuery(this).attr("name"))
+				obj[jQuery(this).attr("name")] = jQuery(this).val();
+		});
+		return obj;
+	},
+	insert_after_panel_info: function(texto, efecto, tipo){
+        var result = '<div class="panel panel-'+tipo+'">'+
+                     '   <div class="panel-heading">'+
+                     '       <h3 class="panel-title"> '+texto+' </h3>'+
+                     '       <span class="pull-right clickable" data-effect="'+efecto+'"><i class="glyphicon glyphicon-remove"></i></span>'+
+                     '   </div>'+
+                     '</div>';
+                    
+        jQuery(this).after(result);
+    }
 });
--- wp_info_panels.php
+++ wp_info_panels.php
@@ -9,9 +9,10 @@ License: GNU General Public License v2 or later
 */
 
 function myplugin_custom_scripts(){
-    wp_enqueue_script( 'wp_info_panels_admin_script', plugins_url("js/wp_info_panels_admin.js", __FILE__));
-    wp_enqueue_style( 'wp_info_panels_admin_script', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
-    wp_enqueue_style( 'wp_info_panels_admin_style', plugins_url("css/wp_info_panels_admin.css", __FILE__) );
+ 	wp_enqueue_script('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js');
+    wp_enqueue_script('wp_info_panels_admin_script', plugins_url("js/wp_info_panels_admin.js", __FILE__), array('jquery-ui-core'), 1.0, true);
+    wp_enqueue_style('wp_info_panels_admin_script', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
+    wp_enqueue_style('wp_info_panels_admin_style', plugins_url("css/wp_info_panels_admin.css", __FILE__) );
 }
 
 add_action('admin_enqueue_scripts', 'myplugin_custom_scripts');
@@ -27,6 +28,17 @@ function add_menu_wp_info_panels(){
 }
 
 function front_banner_config(){
+	global $wpdb;
+	$data = $_POST["form"];
+	$sql = "SELECT * FROM {$wpdb->prefix}wp_info_panels_groups";
+	$groups = $wpdb->get_results($sql);
+
+	$groups_tag = '<div class="list-group groups">';
+	foreach ($groups as $group) {
+		$groups_tag .= '<a class="list-group-item" data-id="'.$group->id.'" data-sizex="'.$group->sizex.'" data-sizey="'.$group->sizey.'">'.$group->name.'</a>';
+	}
+	$groups_tag .= '</div>';
+
 	echo <<<EOF
 	<div class="container container-full">
 		<h2 class="page-header">Wordpress Info Panels</h2>
@@ -35,26 +47,64 @@ function front_banner_config(){
 				<p> Esta herramienta permite configurar los paneles de información de la aplicación </p>
 			</div>
 			<div class="col-sm-12">
-				<h2 class="page-header">Paneles <button class="btn btn-success wip_new_group_panel"> <span class="glyphicon glyphicon-plus"></span> </button> <small style="font-size: .4em;">Pulsa para crear un nuevo panel</small></h2>
-				<div class="wip_new_group_panel_form panel panel-primary">
-					<div class="panel-heading">Creación de paneles</div>
-					<form class="form-inline panel-body" id="wip_new_group">
-						<div class="form-group">
-							<label for="wip_panel_name"> Nombre </label>
-							<input type="text" class="form-control" id="wip_panel_name" name="wip_panel_name" placeholder="Nombre del panel"/> 
+				<h2 class="page-header">Grupos <button class="btn btn-success wip_new_group_panel"> <span class="glyphicon glyphicon-plus"></span> </button> <small style="font-size: .4em;">Pulsa para crear un nuevo panel</small></h2>
+				<div class="wip_new_group_panel_form ">
+					<div class="panel panel-primary">
+						<div class="panel-heading">Creación de grupos</div>
+						<form class="form-inline panel-body wip_update_group">
+							<div class="form-group">
+								<label for="wip_panel_name"> Nombre </label>
+								<input type="text" class="form-control" id="wip_panel_name" name="wip_panel_name" placeholder="Nombre del panel"/> 
+							</div>
+							<div class="form-group">
+								<label for="wip_panel_sizex"> Tamaño horizontal </label>
+								<input type="text" class="form-control" id="wip_panel_sizex" name="wip_panel_sizex" placeholder="(pixeles o porcentaje)"/> 
+							</div>
+							<div class="form-group">
+								<label for="wip_panel_sizey"> Tamaño vertical </label>
+								<input type="text" class="form-control" id="wip_panel_sizey" name="wip_panel_sizey" placeholder="(pixeles o porcentaje)"/> 
+							</div>
+							<input type="hidden" name="wip_update_group" value="0">
+							<div class="form-group">
+								<input type="submit" class="btn btn-success wip_new_panel_add" value="Añadir">
+							</div>
+						</form>
+					</div>
+					<div class="page-header"></div>
+				</div>
+
+				<div class="col-sm-3">
+					<div class="list-group">
+						$groups_tag
+					</div>
+				</div>
+				<div class="col-sm-9">
+					<div class="info_group">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								Información del grupo
+							</div>
+							<form class="form panel-body wip_update_group">
+								<div class="form-group">
+									<label for="wip_panel_name"> Nombre </label>
+									<input type="text" class="form-control" id="wip_panel_name" name="wip_panel_name" placeholder="Nombre del panel"/> 
+								</div>
+								<div class="form-group">
+									<label for="wip_panel_sizex"> Tamaño horizontal </label>
+									<input type="text" class="form-control" id="wip_panel_sizex" name="wip_panel_sizex" placeholder="(pixeles o porcentaje)"/> 
+								</div>
+								<div class="form-group">
+									<label for="wip_panel_sizey"> Tamaño vertical </label>
+									<input type="text" class="form-control" id="wip_panel_sizey" name="wip_panel_sizey" placeholder="(pixeles o porcentaje)"/> 
+								</div>
+								<input type="hidden" name="wip_update_group" value="1">
+								<input type="hidden" name="wip_group_id" id="wip_group_id">
+								<div class="form-group">
+									<input type="submit" class="btn btn-success wip_new_panel_add" value="Actualizar">
+								</div>
+							</form>
 						</div>
-						<div class="form-group">
-							<label for="wip_panel_sizex"> Tamaño horizontal </label>
-							<input type="text" class="form-control" id="wip_panel_sizex" name="wip_panel_sizex" placeholder="(pixeles o porcentaje)"/> 
-						</div>
-						<div class="form-group">
-							<label for="wip_panel_sizey"> Tamaño vertical </label>
-							<input type="text" class="form-control" id="wip_panel_sizey" name="wip_panel_sizey" placeholder="(pixeles o porcentaje)"/> 
-						</div>
-						<div class="form-group">
-							<input type="submit" class="btn btn-success wip_new_panel_add">Añadir</button>
-						</div>
-					</form>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -109,10 +159,30 @@ register_activation_hook( __FILE__, 'wip_install' );
 
 
 //AJAX functions
-add_action("wp_ajax_get_panels", "wip_get_panels");
+add_action("wp_ajax_get_groups", "wip_get_groups");
 
-function wip_get_panels(){
-	echo $_POST["whatever"];
+function wip_get_groups(){
+	global $wpdb;
+	$data = $_POST["form"];
+	$sql = "SELECT * FROM {$wpdb->prefix}wp_info_panels_groups";
+	echo json_encode($wpdb->get_results($sql));
+	wp_die();
+}
+
+add_action("wp_ajax_create_group", "wip_create_group");
+function wip_create_group(){
+	global $wpdb;
+	$data = $_POST["form"];
+
+	if($data['wip_update_group']){
+		$sql = "UPDATE {$wpdb->prefix}wp_info_panels_groups SET name = %s, sizex = %s, sizey = %s where id = %d";
+		echo $wpdb->query($wpdb->prepare($sql, $data['wip_panel_name'], $data['wip_panel_sizex'], $data['wip_panel_sizey'], $data['wip_group_id']));
+	}
+	else{
+		$sql = "INSERT INTO {$wpdb->prefix}wp_info_panels_groups (name, sizex, sizey) VALUES (%s, %s, %s)";
+		echo $wpdb->query($wpdb->prepare($sql, $data['wip_panel_name'], $data['wip_panel_sizex'], $data['wip_panel_sizey']));
+	}
+
 	wp_die();
 }
 
